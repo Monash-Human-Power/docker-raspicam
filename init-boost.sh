b7dc8adc30e31711074d4652cd3e0dc1619ce5a0
Harsil Patel <25992839+harsilspatel@users.noreply.github.com>
2019-09-25 11:59:33 +1000

    add init-boost.sh

---
+++ init-boost.sh
@@ -0,0 +1,9 @@
+# increase state space of RPI
+sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+sudo /sbin/mkswap /var/swap.1
+sudo chmod 600 /var/swap.1
+sudo /sbin/swapon /var/swap.1
+
+
+# install numpy and scipy
+sudo apt-get install python3-numpy python3-scipy
